67cbedb09de776af3d4a9297ef00e4e57b291dc2
Aureliomed <tacuato1@gmail.com>
2025-04-29 15:27:45 -0400

    Actualización

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,9 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
 
 // 🔐 Seguridad
 app.use(helmet());
